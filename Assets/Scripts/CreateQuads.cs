e3cd50630b48e36aaa39132becf54d2fae5316f3
Liam Hurt <liamhurt51@gmail.com>
2018-06-14 13:11:30 -0700

    Creating a combined mesh cube so that the render can batch them together

--- Assets/Scripts/CreateQuads.cs
+++ Assets/Scripts/CreateQuads.cs
@@ -6,7 +6,17 @@ public class CreateQuads : MonoBehaviour {
 
     public Material blockMat;
 
-    void CreateQuad()
+    enum CubeSide
+    {
+        BOTTOM,
+        TOP,
+        LEFT,
+        RIGHT,
+        FRONT,
+        BACK
+    };
+
+    void CreateQuad(CubeSide side)
     {
         Mesh mesh = new Mesh();
 
@@ -21,7 +31,7 @@ public class CreateQuads : MonoBehaviour {
         Vector2 uv01 = new Vector2(0.0f, 1.0f);
         Vector2 uv11 = new Vector2(1.0f, 1.0f);
 
-        // All possible vertices
+        // All possible vertices in a cube
         Vector3 p0 = new Vector3(-0.5f, -0.5f, 0.5f);
         Vector3 p1 = new Vector3(0.5f, -0.5f, 0.5f);
         Vector3 p2 = new Vector3(0.5f, -0.5f, -0.5f);
@@ -31,8 +41,34 @@ public class CreateQuads : MonoBehaviour {
         Vector3 p6 = new Vector3(0.5f, 0.5f, -0.5f);
         Vector3 p7 = new Vector3(-0.5f, 0.5f, -0.5f);
 
-        vertices = new Vector3[] { p4, p5, p1, p0 };
-        normals = new Vector3[] { Vector3.forward, Vector3.forward, Vector3.forward, Vector3.forward };
+        switch(side)
+        {
+            case CubeSide.BOTTOM:
+                vertices = new Vector3[] { p0, p1, p2, p3 };
+                normals = new Vector3[] { Vector3.down, Vector3.down, Vector3.down, Vector3.down };
+                break;
+            case CubeSide.TOP:
+                vertices = new Vector3[] { p7, p6, p5, p4 };
+                normals = new Vector3[] { Vector3.up, Vector3.up, Vector3.up, Vector3.up };
+                break;
+            case CubeSide.LEFT:
+                vertices = new Vector3[] { p7, p4, p0, p3 };
+                normals = new Vector3[] { Vector3.left, Vector3.left, Vector3.left, Vector3.left };
+                break;
+            case CubeSide.RIGHT:
+                vertices = new Vector3[] { p5, p6, p2, p1 };
+                normals = new Vector3[] { Vector3.left, Vector3.left, Vector3.left, Vector3.left };
+                break;
+            case CubeSide.FRONT:
+                vertices = new Vector3[] { p4, p5, p1, p0 };
+                normals = new Vector3[] { Vector3.forward, Vector3.forward, Vector3.forward, Vector3.forward };
+                break;
+            case CubeSide.BACK:
+                vertices = new Vector3[] { p6, p7, p3, p2 };
+                normals = new Vector3[] { Vector3.forward, Vector3.forward, Vector3.forward, Vector3.forward };
+                break;
+        }
+
         uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
         triangles = new int[] { 3, 1, 0, 3, 2, 1 };
 
@@ -43,7 +79,7 @@ public class CreateQuads : MonoBehaviour {
         mesh.triangles = triangles;
         mesh.RecalculateBounds();
 
-        GameObject quad = new GameObject("quad");
+        GameObject quad = new GameObject("Quad");
         MeshFilter meshFilter = (MeshFilter)quad.AddComponent(typeof(MeshFilter));
         MeshRenderer renderer = quad.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
 
@@ -52,9 +88,48 @@ public class CreateQuads : MonoBehaviour {
         renderer.material = blockMat;
     }
 
+    void CombineQuads()
+    {
+        // Combine all children meshes
+        int i = 0;
+        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+
+        while (i < meshFilters.Length)
+        {
+            combine[i].mesh = meshFilters[i].sharedMesh;
+            combine[i].transform = meshFilters[i++].transform.localToWorldMatrix;
+        }
+
+        // Create a new mesh on the parent object
+        MeshFilter mf = (MeshFilter)this.gameObject.AddComponent(typeof(MeshFilter));
+        // Create a renderer for the parent
+        MeshRenderer renderer = this.gameObject.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
+
+        mf.mesh = new Mesh();
+        // Add combined meshes on children as the parents mesh
+        mf.mesh.CombineMeshes(combine);
+
+        renderer.material = blockMat;
+
+        // Delete all uncombined children
+        foreach (Transform quad in this.transform) Destroy(quad.gameObject);
+    }
+
+    void CreateCube()
+    {
+        CreateQuad(CubeSide.FRONT);
+        CreateQuad(CubeSide.BACK);
+        CreateQuad(CubeSide.TOP);
+        CreateQuad(CubeSide.BOTTOM);
+        CreateQuad(CubeSide.LEFT);
+        CreateQuad(CubeSide.RIGHT);
+        CombineQuads();
+    }
+
 	// Use this for initialization
 	void Start () {
-        CreateQuad();
+        CreateCube();
 	}
 	
 	// Update is called once per frame
