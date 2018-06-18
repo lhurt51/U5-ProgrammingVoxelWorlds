7a37711372024c5d482171a9c1226a3b0cf1fcba
Liam Hurt <liamhurt51@gmail.com>
2018-06-18 12:56:20 -0700

    adding perlin noise to the generation of each chunk

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utils {
+
+    static int maxHeight = 150;
+    static float smooth = 0.01f;
+    static int octaves = 4;
+    static float persistence = 0.5f;
+
+    static float Map(float newMin, float newMax, float origMin, float origMax, float val)
+    {
+        return Mathf.Lerp(newMin, newMax, Mathf.InverseLerp(origMin, origMax, val));
+    }
+
+    static float fBM(float x, float z, int oct, float pers)
+    {
+        float total = 0.0f;
+        float freq = 1.0f;
+        float amp = 1.0f;
+        float maxVal = 0.0f;
+
+        for (int i = 0; i < oct; i++)
+        {
+            total += Mathf.PerlinNoise(x * freq, z * freq) * amp;
+            maxVal += amp;
+            amp *= pers;
+            freq *= 2;
+        }
+
+        return total / maxVal;
+    }
+
+    public static int GenHeight(float x, float z)
+    {
+        return (int)Map(0.0f, maxHeight, 0.0f, 1.0f, fBM(x * smooth, z * smooth, octaves, persistence));
+    }
+
+}
